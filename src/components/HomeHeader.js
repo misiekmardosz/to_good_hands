128615eb51230f99f3225fad8f96ca47b30ac087
misiekmardosz <michal.mardosz@gmail.com>
2022-03-23 14:35:58 +0100

    structure done and header in progress

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,10 +1,30 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import  homeHero from "../assets/Home-Hero-Image.jpg"
+// import * as Scroll from 'react-scroll';
+// import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+
 
 const HomeHeader = () => {
     return(
         <>
-            {/*<Link to="/oddaj-rzeczy">oddaj-rzeczy</Link>*/}
+            <header className="header">
+                {/*<img src={homeHero} className="main--photo"/>*/}
+                <div className="main--photo"></div>
+                <div className="nav--container">
+                    <div className="login--panel">
+                        <Link to="/logowanie">Zaloguj</Link>
+                        <Link to="/rejestracja">Załóż Konto</Link>
+                    </div>
+                    <nav className="nav">
+                        <a href="#" className="nav--link">Start</a>
+                        <a href="#" className="nav--link">O co chodzi?</a>
+                        <a href="#" className="nav--link">O nas</a>
+                        <a href="#" className="nav--link">Fundacja i organizacje</a>
+                        <a href="#" className="nav--link">Kontakt</a>
+                    </nav>
+                </div>
+            </header>
         </>
     )
 }
