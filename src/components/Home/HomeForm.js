35fa7153a54bbf3520fcb4a44c078f32b0955d15
misiekmardosz <michal.mardosz@gmail.com>
2022-03-24 13:09:26 +0100

    footer alsmost done

--- src/components/Home/HomeForm.js
+++ src/components/Home/HomeForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {Link as Scroll} from "react-scroll";
+import facebook from "../../assets/Facebook.svg"
+import instagram from "../../assets/Instagram.svg"
 
 const HomeForm = () => {
   return(
@@ -9,15 +11,31 @@ const HomeForm = () => {
             <div className="form--photo"></div>
             <div className={"form--box"}>
             <h2 className={"form--title"}>Skontaktuj się z nami</h2>
-            <form className={"form--form"}>
-              <input className={"form--input"} placeholder={"Krzysztof"}/>
-              <input className={"form--input"} placeholder={"abc@xyz.com"}/>
-              <textarea>Wpisz swoja wiadomość</textarea>
-              <button>Wyślij</button>
+            <form className={"form"}>
+                <div className={"inputs"}>
+                    <div className={"input"}>
+                        <h3>wpisz swoje imie</h3>
+                        <input className={"form--input"} placeholder={"Krzysztof"}/>
+                    </div>
+                    <div className={"input"}>
+                        <h3>wpisz swoj email</h3>
+                        <input className={"form--input"} placeholder={"abc@xyz.com"}/>
+                    </div>
+                </div>
+                <div className={"textarea"}>
+                    <h3>Wpisz swoja wiadomość</h3>
+                    <textarea placeholder={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}/>
+                </div>
+              <button className={"send--button"}>Wyślij</button>
             </form>
           </div>
         </div>
-          <div className={'footer'}></div>
+          <div className={'footer'}>
+              <h5>Copyright by Coders Lab</h5>
+              <a href="#"><img src={instagram}/></a>
+              <a href="#"><img src={facebook}/></a>
+
+          </div>
       </footer>
 
   )
