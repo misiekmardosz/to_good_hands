4411706d21135d2261b6d4aa794eec27cb0c63e4
misiekmardosz <michal.mardosz@gmail.com>
2022-03-29 20:20:23 +0200

    validation email starterd

--- src/components/Home/HomeForm.js
+++ src/components/Home/HomeForm.js
@@ -6,13 +6,23 @@ import instagram from "../../assets/Instagram.svg"
 
 const HomeForm = () => {
     const [email, setEmail] = useState('')
-    const handleEmail = () => {
-        setEmail(handleEmail)
+    const [name, setName] = useState('')
+    const [message, setMessage] = useState('')
+
+    const ValidMail = () => {
+      if(email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
+      return<h4>good</h4>
+        else
+            return <></>
     }
+    // const handleFormSubmit = () => {
+    //   return
+    // }
 
 
 
-  return(
+
+    return(
       <footer className="footer--form" title="section6" id="section6">
         <div className="form--container">
             <div className="form--photo"></div>
@@ -22,19 +32,25 @@ const HomeForm = () => {
                 <div className={"inputs"}>
                     <div className={"input"}>
                         <h3>wpisz swoje imie</h3>
-                        <input className={"form--input"} placeholder={"Krzysztof"}/>
-                        <input type="text" className={"form--input"} placeholder='0'onChange={e => handleEmail(e.target.value)}/>
+                        {/*<input className={"form--input"} placeholder={"Krzysztof"}/>*/}
+                        <input type="text" className={"form--input"} placeholder='Krzysztof'onChange={e => setName(e.target.value)}/>
                     </div>
                     <div className={"input"}>
                         <h3>wpisz swoj email</h3>
-                        <input className={"form--input"} placeholder={"abc@xyz.com"}/>
+                        {/*<input className={"form--input"} placeholder={"abc@xyz.com"}/>*/}
+                        <input type="text" className={"form--input"} placeholder='abc@xyz.com'onChange={e => setEmail(e.target.value)}/>
+                        <ValidMail/>
                     </div>
                 </div>
                 <div className={"textarea"}>
                     <h3>Wpisz swoja wiadomość</h3>
-                    <textarea placeholder={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}/>
+                    <textarea onChange={e => setMessage(e.target.value)} placeholder={"Lorem ipsum dolor sit amet," +
+                        " consectetur adipiscing elit," +
+                        " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip " +
+                        "ex ea commodo consequat."}/>
                 </div>
-              <button className={"send--button"}>Wyślij</button>
+              <button className={"send--button"} onClick={"submit"}>Wyślij</button>
             </form>
           </div>
             <a href="#"><img src={instagram}/></a>
