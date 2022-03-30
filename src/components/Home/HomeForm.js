6ba7bee1346268fbf2fde0a73668ccfca2632a14
misiekmardosz <michal.mardosz@gmail.com>
2022-03-30 10:22:11 +0200

    form validation comunicats

--- src/components/Home/HomeForm.js
+++ src/components/Home/HomeForm.js
@@ -9,16 +9,40 @@ const HomeForm = () => {
     const [name, setName] = useState('')
     const [message, setMessage] = useState('')
 
-    const ValidMail = () => {
-      if(email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
-      return<h4>good</h4>
-        else
-            return <></>
-    }
+    // const ValidMail = () => {
+    //   if(email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))
+    //       return <></>
+    //     else if (email !== "")
+    //         return <h4 className={"mail--valid"}>podany email jest nieprawidłowy</h4>
+    //     else
+    //         return <></>
+    // }
     // const handleFormSubmit = () => {
     //   return
     // }
-
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await fetch("https://fer-api.coderslab.pl/v1/portfolio/contact", {
+                method: "POST",
+                body: JSON.stringify({
+                    location: email,
+                    msg: message,
+                    param: name
+                }),
+            });
+            let resJson = await res.json();
+            if (res.status === 200) {
+                setName("");
+                setEmail("");
+                setMessage("User created successfully");
+            } else {
+                setMessage("Some error occured");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
 
 
@@ -28,7 +52,7 @@ const HomeForm = () => {
             <div className="form--photo"></div>
             <div className={"form--box"}>
             <h2 className={"form--title"}>Skontaktuj się z nami</h2>
-            <form className={"form"}>
+            <form className={"form"} onSubmit={handleSubmit}>
                 <div className={"inputs"}>
                     <div className={"input"}>
                         <h3>wpisz swoje imie</h3>
@@ -39,7 +63,7 @@ const HomeForm = () => {
                         <h3>wpisz swoj email</h3>
                         {/*<input className={"form--input"} placeholder={"abc@xyz.com"}/>*/}
                         <input type="text" className={"form--input"} placeholder='abc@xyz.com'onChange={e => setEmail(e.target.value)}/>
-                        <ValidMail/>
+                        {/*<ValidMail/>*/}
                     </div>
                 </div>
                 <div className={"textarea"}>
